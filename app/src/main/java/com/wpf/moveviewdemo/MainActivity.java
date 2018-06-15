9a5ceab86028b474f62cd2efee83258d125af33a
王朋飞 <wazsj6457@gmail.com>
2018-06-15 14:51:13 +0800

    大升级2.0.

--- app/src/main/java/com/wpf/moveviewdemo/MainActivity.java
+++ app/src/main/java/com/wpf/moveviewdemo/MainActivity.java
@@ -1,13 +1,26 @@
 package com.wpf.moveviewdemo;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.wpf.moveview.MoveView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = findViewById(R.id.list_view);
+        list.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        SimpleAdapter adapter = new SimpleAdapter();
+        adapter.setOrientation(LinearLayoutManager.HORIZONTAL);
+        list.setAdapter(adapter);
     }
 }
